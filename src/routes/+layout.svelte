ac7a9b94cb7c4ed1b77d49de8f36bca037b07237
Filonzi Jacopo <st10357@iismarconipieralisi.it>
2023-12-15 14:55:54 +0100

    up

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Head from './Head.svelte'
+
+    function toggleDarkmode() {
+        // document.getElementsByTagName("html")[0].setAttribute('data-bs-theme', 'dark')
+        document.getElementsByTagName("html")[0].setAttribute('data-bs-theme', 'dark')
+    }
+
+</script>
+<Head />
+
+<button>
+
+</button>
+
+<slot />
